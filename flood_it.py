13a58d18bce39944a2751b1f110e7871df72b081
Loïc VIMIEN <loic.vimien@etu.uca.fr>
2020-02-26 19:15:52 +0100

    Suppression d'une variable inutile

--- flood_it.py
+++ flood_it.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-# -*-coding:utf-8 -*
-
 from tkinter import *
 from Gmatrix import GMatrix
 
@@ -8,6 +5,6 @@ window = Tk()
 window.title("Flood it")
 icon = PhotoImage(file='icon.png')
 window.iconphoto(True, icon)
-M = GMatrix(window, 10, 10, 50)
+GMatrix(window, 10, 10, 50)
 
 window.mainloop()
