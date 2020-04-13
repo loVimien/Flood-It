c7124b439299450cfdb5b97645d2de2fa2a495c8
Lilian Hiault <lilian.hiault@laposte.net>
2020-04-13 14:50:46 +0200

    Changement mineur fonction win

--- Gmatrix.py
+++ Gmatrix.py
@@ -16,7 +16,7 @@ class GMatrix:
             self._mat.append(line)
         self._moves = -1
         self._currSet = [[self._mat[0][0].color, 0, 0]]
-        
+
         self._textFont = font.Font(family='Helvetica', size=12, weight='bold')
         self._textMoves = StringVar()
         self._textMoves.set("Coups joués : 0")
@@ -24,12 +24,13 @@ class GMatrix:
         self._labelMoves.grid(row = 0, column = 0, columnspan=5)
         self._textWin = None
 
+
         self.updateSet(self[(0, 0)])
 
     def __getitem__(self, coord):
         x, y = coord
         return self._mat[x][y].color
-    
+
     def updateSet(self, colorToUpdate):
         i = 0
         while(i < len(self._currSet)):
@@ -56,26 +57,33 @@ class GMatrix:
             self._currSet[i][0] = colorToUpdate
         self.updateGraphicMatrix()
 
-        self.win() # Quitte si on a gagné sinon renvoie faux
+        if self.isFill():
+            self.win() # Quitte si on a gagné sinon renvoie faux
 
     def updateGraphicMatrix(self):
         for k in range(len(self._currSet)):
             currI = self._currSet[k][1]
             currJ = self._currSet[k][2]
             self._mat[currI][currJ].color = self._currSet[k][0]
-    
-    def win(self):
+
+    def isFill(self):
         if len(self._currSet) == len(self._mat) * len(self._mat[0]):
         # Si le nombre de carré de la nouvelle couleur est égal au nombre total alors toute la grille est remplie
         # Le Flood It est rempli d'une seule couleur
+            return True
+        else:
+            return False
+
+    def win(self):
             self.destroySquares()
             self._textWin = Label(self._window, text="Vous avez gagné", font=self._textFont)
             self._textWin.grid(row=1, column=0, columnspan=10)
-        return False
+
     def destroySquares(self):
         for i in self._mat:
             for j in i:
                 j.destroy()
+
     def destroyAll(self):
         if self._textWin != None:
             self._textWin.destroy()
