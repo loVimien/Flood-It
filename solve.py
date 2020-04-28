8c3356615a607ffe2c4e705e5908547c2dd9dcc4
Lilian Hiault <34962237+LilianHiault@users.noreply.github.com>
2020-04-28 16:30:18 +0200

    Modif solve.py : ajout greedySolve (non fonctionnel), SaveMatrix et restoreMatrix

--- solve.py
+++ solve.py
@@ -5,18 +5,47 @@ from random import choice
 
 
 class Solve:
-    def __init__(self, matrix, currSet):
+    def __init__(self, matrix, currSet, moves):
         self._mat = matrix.copy() # Matrice des couleurs et leur position
-        self._currSet = currSet.copy()
-        self._moves = 0
+        self._currSet = currSet.copy() # liste des couleurs à mettre à jour
+        self._moves = moves # nombre de coups
 
+        self.saveMat = matrix.copy()
+        self.saveCurrSet = currSet.copy()
+        self.saveMoves = moves
+
+
+    def saveMatrix(self):
+        self.saveMat = self._mat.copy()
+        self.saveCurrSet = self._currSet.copy()
+        self.saveMoves = self._moves
+
+    def restoreMatrix(self):
+        self._mat = self.saveMat
+        self._currSet = self.saveCurrSet
+        self._moves = self.saveMoves
 
     def randomColor(self):
-        """Choisis une couleur aléatoire"""
+        """Renvoie une couleur aléatoire"""
         return choice(list(PossibleColor))
 
+    def greedyColor(self):
+        """Renvoie la couleur qui maximise le remplissage"""
+        max = 0
+        self.saveMatrix()
+        maxColor = choice(list(PossibleColor))
+        for color in list(PossibleColor):
+            self.updateMat(color)
+            if len(self._currSet) > max:
+                max = len(self._currSet)
+                maxColor = color
+            self.restoreMatrix()
+        # print(maxColor)
+        return maxColor
+
+
     def updateMat(self, colorToUpdate):
-        """Résoud le jeu et retoune le nombre de coups"""
+        """Met à jour ma matrice en fonction d'une couleur choisie"""
         i = 0
         while(i < len(self._currSet)):
             currI = self._currSet[i][1]
@@ -38,6 +67,7 @@ class Solve:
 
 
     def isFill(self):
+        """Renvoie un booléen : vrai si le Flood-it est rempli d'une seule couleur, faux sinon"""
         if len(self._currSet) == len(self._mat) * len(self._mat[0]):
         # Si le nombre de carré de la nouvelle couleur est égal au nombre total alors toute la grille est remplie
         # Le Flood It est rempli d'une seule couleur
@@ -46,7 +76,22 @@ class Solve:
             return False
 
 
-    def solveMat(self):
+    def randSolve(self):
+        """Méthode de résolution aléatoire"""
+        self.saveMatrix()
         while not self.isFill():
             self.updateMat(self.randomColor())
-        return self._moves
+        # On remet tout dans la situation initiale pour pouvoir utiliser d'autres algorithmes de résolution
+        nbMoves = self._moves
+        self.restoreMatrix()
+        return nbMoves
+
+    def greedySolve(self):
+        """Méthode de résolution où à chaque tour, on colore un maximum de cases"""
+        self.saveMatrix()
+        while not self.isFill():
+            self.updateMat(self.greedyColor())
+        # On remet tout dans la situation initiale pour pouvoir utiliser d'autres algorithmes de résolution
+        nbMoves = self._moves
+        self.restoreMatrix()
+        return nbMoves
