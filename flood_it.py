8ed8d099803e3aac9a8ffec2736b262e4fecc0a1
Loïc VIMIEN <loic.vimien@etu.uca.fr>
2020-06-04 15:37:00 +0200

    Ajout de variables contenant le nombre de lignes et de colonnes aisni que la dimension des carrés

--- flood_it.py
+++ flood_it.py
@@ -1,10 +1,16 @@
 mat = None
+nbLines = 10
+nbColumns = 10
+squareDim = 50
 
 def main():
     def new_grid():
         global mat
+        global nbLines
+        global nbColumns
+        global squareDim
         mat.destroyAll()
-        mat = GMatrix(window, 10, 10, 2, 0, 50)
+        mat = GMatrix(window, nbLines, nbColumns, 2, 0, squareDim)
         mat.display(2)
 
     def reset():
@@ -12,16 +18,19 @@ def main():
         mat.resetMatrix()
 
     global mat
+    global nbLines
+    global nbColumns
+    global squareDim
     window = Tk()
     window.title("Flood it")
     icon = PhotoImage(file='icon.png')
     window.iconphoto(True, icon)
-    mat = GMatrix(window, 10, 10, 2, 0, 50)
+    mat = GMatrix(window, nbLines, nbColumns, 2, 0, squareDim)
     mat.display(2)
     b_new = Button(window, text="Nouvelle grille", command=new_grid)
     b_reset = Button(window, text="Recommencer", command=reset)
-    b_new.grid(row=0, column=8, columnspan=2)
-    b_reset.grid(row=1, column=8, columnspan=2)
+    b_new.grid(row=0, column=int(0.8*nbColumns), columnspan=2)
+    b_reset.grid(row=1, column=int(0.8*nbColumns), columnspan=2)
 
     window.mainloop()
 
